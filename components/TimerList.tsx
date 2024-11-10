23fa607360fad0326b2ae05a3a3289877cf07982
Personal <personal@admins-MacBook-Pro-5.local>
2024-11-10 15:46:57 +0530

    initial setup

---
+++ components/TimerList.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+const TimerList = () => {
+  return (
+    <Text>TimerList</Text>
+  )
+}
+
+export default TimerList
---
+++ components/TimerPicker.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+const TimerPicker = () => {
+  return (
+    <Text>TimerPicker</Text>
+  )
+}
+
+export default TimerPicker
